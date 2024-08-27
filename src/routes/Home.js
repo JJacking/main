b147485e9c81a60f67d1b394f1ecc21566533126
JJackson <112931780+JJacking@users.noreply.github.com>
2024-08-28 03:14:59 +0900

    2day
    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import BoardList from "./BoardList";
 import img1 from '../image/dog_1.jpg';
-import img2 from '../image/dog_2.jpg'
-import img3 from '../image/dog_3.jpg'
 
 
 const Home = () => {
+
   return (
     <div className='menubar'>
+
         <ul>
             <li>
                 <a href='/board'>
@@ -19,14 +19,14 @@ const Home = () => {
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img2} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img3} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
@@ -40,14 +40,14 @@ const Home = () => {
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img2} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img3} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
@@ -61,14 +61,14 @@ const Home = () => {
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img2} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
             </li>
             <li>
                 <a href='/board'>
-                    <img src={img3} width='100%'/>
+                    <img src={img1} width='100%'/>
                     <p>test</p>
                 </a>
                 <p>설명</p>
